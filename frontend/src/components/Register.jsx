d997cc161e51f3edcdc91b4eed8fde759a7fc851
sharanu24 <167069255+sharanu24@users.noreply.github.com>
2025-05-06 16:20:49 +0530

    Update Register.jsx

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -11,7 +11,7 @@ export default function Register() {
 
   const handleRegister = async (e) => {
     e.preventDefault();
-    const res = await fetch('http://localhost:5000/api/auth/register', {
+    const res = await fetch('https://newlogin-page-backend.onrender.com/api/auth/register', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ userId, password }),
