6ca1bfac7d24434494288ce25ddaf6dc240b389a
sharanu24 <167069255+sharanu24@users.noreply.github.com>
2025-05-06 16:20:29 +0530

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -12,7 +12,7 @@ export default function Login() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const res = await fetch('http://localhost:5000/api/auth/login', {
+    const res = await fetch('https://newlogin-page-backend.onrender.com/api/auth/login', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ userId, password }),
